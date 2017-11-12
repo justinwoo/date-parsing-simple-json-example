e6b556313364657bf264f0e027984067b0636f85
justinwoo <moomoowoo@gmail.com>
2017-11-12 21:52:30 +0200

    update names

--- src/Main.purs
+++ src/Main.purs
@@ -13,22 +13,22 @@ import Data.Record (set)
 import Simple.JSON (readJSON)
 import Type.Prelude (SProxy(..))
 
-type MyShit =
+type MyThing =
   { a :: String
   , b :: JSDate
   }
 
-readJSONMyShit :: forall e
+readJSONMyThing :: forall e
    . String
-  -> Eff ( locale :: LOCALE | e) (Either (NonEmptyList ForeignError) MyShit)
-readJSONMyShit jsonString = runExceptT do
+  -> Eff ( locale :: LOCALE | e) (Either (NonEmptyList ForeignError) MyThing)
+readJSONMyThing jsonString = runExceptT do
   record <- except $ readJSON jsonString
 
   -- knows that record.b should be parsed as `String` because of this usage
   jsDate <- lift $ parse record.b
 
   -- set record.b from `String` to `JSDate`
-  pure $ set bProxy jsDate record :: MyShit
+  pure $ set bProxy jsDate record :: MyThing
   where
     bProxy = SProxy :: SProxy "b"
 
@@ -58,16 +58,16 @@ testJson3 = """
 
 main :: forall e. Eff (locale :: LOCALE, console :: CONSOLE | e) Unit
 main = do
-  logDate =<< readJSONMyShit testJson1
-  logDate =<< readJSONMyShit testJson2
-  logDate =<< readJSONMyShit testJson3
+  logDate =<< readJSONMyThing testJson1
+  logDate =<< readJSONMyThing testJson2
+  logDate =<< readJSONMyThing testJson3
 
   -- output
   -- Sat Nov 04 2017
   -- Sat Nov 04 2017
   -- Sat Nov 04 2017
   where
-    logDate (Right myShit) = do
-      log $ toDateString myShit.b
+    logDate (Right myThing) = do
+      log $ toDateString myThing.b
     logDate (Left err) = do
       log $ "failed to parse: " <> show err
